21468bdfca5e98c77c6019776ceacd2ee3037a99
allenxing <allenxing@tencent.com>
2018-12-17 21:10:58 +0800

    first commit

---
+++ src/pages/options/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div>options</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/pages/popup/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div>pop</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    }
+  }
+</script>
+
+<style>
+</style>
